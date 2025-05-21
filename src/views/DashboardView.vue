36930d2ed47307ff56692a829d6a0e4fd93f4d82
Veronica Giraldo Garrido <veronica.giraldo.garrido@gmail.com>
2025-05-21 21:57:54 +0200

    Fotos de todos los continentes en dashboard.

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -35,27 +35,27 @@
         <h1 class="text-2xl font-bold mt-10">América</h1>
         <div class="grid grid-cols-3 mt-6 gap-4">
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/america/nuevayork.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Nueva York (Estados Unidos)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/america/ottawa.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Ottawa (Canadá)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/america/mexico.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Ciudad de México (México)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/america/buenosaires.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Buenos Aires (Argentina)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/america/janeiro.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Río de Janeiro (Brasil)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/america/lima.png" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Lima (Perú)</figcaption>
             </figure>            
         </div>
@@ -63,27 +63,27 @@
         <h1 class="text-2xl font-bold mt-10">Asia</h1>
         <div class="grid grid-cols-3 mt-6 gap-4">
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/asia/pekin.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Pekín (China)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/asia/hongkong.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Hong Kong (China)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/asia/bangkok.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Bangkok (Tailandia)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/asia/taipei.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Taipéi (Taiwán)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/asia/seul.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Seúl (Corea del Sur)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/asia/tokio.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Tokio (Japón)</figcaption>
             </figure>            
         </div>
@@ -92,27 +92,27 @@
         <div class="grid grid-cols-3 mt-6 gap-4">
             
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/africa/rabat.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Rabat (Marruecos)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/africa/argel.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Argel (Argelia)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/africa/cairo.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">El Cairo (Egipto)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/africa/abiyan.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Abiyán (Costa de Marfil)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/africa/cabo.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Ciudad del Cabo (Sudáfrica)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/africa/antana.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Antananarivo (Madagascar)</figcaption>
             </figure>            
         </div>
@@ -120,27 +120,27 @@
         <h1 class="text-2xl font-bold mt-10">Oceanía</h1>
         <div class="grid grid-cols-3 mt-6 gap-4">
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/oceania/sidney.jpeg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Sídney (Australia)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/oceania/wellington.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Wellington (Nueva Zelanda)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/oceania/moresby.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Port Moresby (Nueva Guinea)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/oceania/numea.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Numea (Nueva Caledonia)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/oceania/suva.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Suva (Islas Fiyi)</figcaption>
             </figure>
             <figure class="max-w-lg">
-            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/BG2.jpg" alt="image description">
+            <img class="h-auto max-w-full transition-transform duration-300 ease-in-out hover:scale-105 hover:opacity-50" src="@/assets/images/oceania/apia.jpg" alt="image description">
             <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Apia (Samoa)</figcaption>
             </figure>            
         </div>
